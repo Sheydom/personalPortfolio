cea8c93428b1401612bd3b33ddc996273e9e62f9
Dom <dominic.knabe@gmail.com>
2025-03-23 19:23:33 +1100

    created dist folder with npm and dependencies for optimization

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+(()=>{var e={630:()=>{document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, span, li, a").forEach((e=>e.style.fontFamily="Merriweather"),"serif")}},r={};function t(o){var a=r[o];if(void 0!==a)return a.exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}t.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return t.d(r,{a:r}),r},t.d=(e,r)=>{for(var o in r)t.o(r,o)&&!t.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:r[o]})},t.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";t(630)})()})();
+//# sourceMappingURL=main.js.map
---
+++ index.js
@@ -0,0 +1,6 @@
+//webpack entry point
+import "./javascript"
+
+//import css
+import "./style.css"
+
